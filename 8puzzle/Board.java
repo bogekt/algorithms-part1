d12db0e400606f0a3b574613573f497d3b28e5db
Eugene Borys <bogekt@gmail.com>
2019-12-12 10:17:18 +0200

    add initializer

--- 8puzzle/Board.java
+++ 8puzzle/Board.java
@@ -4,6 +4,7 @@
  *  Description:
  **************************************************************************** */
 
+import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.StdRandom;
 
@@ -15,11 +16,69 @@ public class Board {
     private final char[] singleDimensionTiles;
     private final String boardString;
     private final boolean isGoal;
+    private final int blankTitleIndex;
     private final int hamming;
     private final int manhattan;
 
-    private static boolean isBlankTile(char value) {
-        return value == 0;
+    class BoardInitializer {
+        private int n;
+        private int n2;
+        private boolean isGoal;
+        private int hammingAccumulator;
+        private int manhattanAccumulator;
+        private int blankTitleIndex;
+        private char[] singleDimensionTiles;
+        // 128 is max n + whitespace = 4 symbols
+        // 128*128 is 5 symbols + whitespace = 6 symbols
+        // n2 * 6 + 4
+        private StringBuilder sb;
+        private boolean neighbor;
+
+        BoardInitializer(int n, boolean neighbor) {
+            this.n = n;
+            this.n2 = n * n;
+            this.neighbor = neighbor;
+            sb = new StringBuilder();
+            sb.append(n).append(System.lineSeparator());
+            isGoal = true;
+            blankTitleIndex = 0;
+            // todo
+            singleDimensionTiles = new char[n2];
+
+            if (neighbor) return;
+
+            hammingAccumulator = 0;
+            manhattanAccumulator = 0;
+        }
+
+        private void tile(int tile, int i, int j) {
+            if (tile > 127) throw new IllegalArgumentException();
+
+            int k = toSingleDimension(i, j, n);
+            char value = (char) tile;
+
+            singleDimensionTiles[k] = value;
+
+            if (isBlankTile(value)) {
+                blankTitleIndex = k;
+            }
+            else {
+                if (value != k + 1) {
+                    isGoal = false;
+
+                    if (!neighbor) {
+                        hammingAccumulator++;
+                        manhattanAccumulator += Math.abs(i - toRow(value, n))
+                                + Math.abs(j + 1 - toCol(value, n));
+                    }
+                }
+                if (isGoal && k > 0) isGoal = singleDimensionTiles[k - 1] < value;
+            }
+            // string
+            sb.append(" ").append(tile);
+
+            if (j == n - 1) sb.append(System.lineSeparator());
+        }
     }
 
     // create a board from an n-by-n array of tiles,
@@ -27,63 +86,41 @@ public class Board {
     public Board(int[][] tiles) {
         if (tiles == null) throw new IllegalArgumentException();
 
-        n = tiles.length;
-        n2 = n * n;
+        BoardInitializer initializer = new BoardInitializer(tiles.length, false);
 
-        singleDimensionTiles = new char[n2];
-        boolean canBeGoal = true;
-        int hammingCounter = 0;
-        int manhattanCounter = 0;
-        // 128 is max n + whitespace = 4 symbols
-        // 128*128 is 5 symbols + whitespace = 6 symbols
-        // n2 * 6 + 4
-        StringBuilder sb = new StringBuilder();
-        sb.append(n).append(System.lineSeparator());
+        for (int i = 0; i < tiles.length; i++)
+            for (int j = 0; j < tiles[i].length; j++)
+                initializer.tile(tiles[i][j], i, j);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (j >= tiles[i].length) throw new IllegalArgumentException();
-
-                int origin = tiles[i][j];
-                if (origin > 127) throw new IllegalArgumentException();
-
-                int k = toSingleDimension(i, j);
-                char value = (char) origin;
-                singleDimensionTiles[k] = value;
-
-                if (!isBlankTile(value)) {
-                    if (value != k + 1) {
-                        canBeGoal = false;
-                        hammingCounter++;
-                        manhattanCounter += Math.abs(i - toRow(value))
-                                + Math.abs(j + 1 - toCol(value));
-                    }
-                    if (canBeGoal && k > 0) canBeGoal = singleDimensionTiles[k - 1] < value;
-                }
-                else if (isBlankTile(value)) {
-                    // TODO neighbors
-                }
-                // string
-                sb.append(" ").append(origin);
-            }
-            sb.append(System.lineSeparator());
-        }
-
-        isGoal = canBeGoal;
-        hamming = hammingCounter;
-        manhattan = manhattanCounter;
-        boardString = sb.toString();
+        n = initializer.n;
+        n2 = initializer.n2;
+        singleDimensionTiles = initializer.singleDimensionTiles.clone();
+        isGoal = initializer.isGoal;
+        hamming = initializer.hammingAccumulator;
+        manhattan = initializer.manhattanAccumulator;
+        blankTitleIndex = initializer.blankTitleIndex;
+        boardString = initializer.sb.toString();
     }
 
-    // copy constructor
-    private Board(Board other) {
-        n = other.n;
-        n2 = other.n2;
-        singleDimensionTiles = other.singleDimensionTiles.clone();
-        isGoal = other.isGoal;
-        hamming = other.hamming;
-        manhattan = other.manhattan;
-        boardString = other.boardString;
+    // neighbor constructor
+    public Board(char[] singleDimensionTiles, int hamming, int manhattan) {
+        int n = (int) Math.sqrt(singleDimensionTiles.length);
+        BoardInitializer initializer = new BoardInitializer(n, true);
+
+        for (int k = 0; k < singleDimensionTiles.length; k++) {
+            int i = toRow(k, n);
+            int j = toCol(k, n);
+            initializer.tile(singleDimensionTiles[k], i, j);
+        }
+
+        n2 = initializer.n2;
+        isGoal = initializer.isGoal;
+        blankTitleIndex = initializer.blankTitleIndex;
+        boardString = initializer.sb.toString();
+        this.n = initializer.n;
+        this.singleDimensionTiles = initializer.singleDimensionTiles.clone();
+        this.hamming = hamming;
+        this.manhattan = manhattan;
     }
 
     // string representation of this board
@@ -123,7 +160,23 @@ public class Board {
 
     // all neighboring boards
     public Iterable<Board> neighbors() {
-        return null;
+        // todo
+        Bag<Board> neighbors = new Bag<>();
+        int col = toCol(blankTitleIndex, singleDimensionTiles.length);
+        int row = toRow(blankTitleIndex, singleDimensionTiles.length);
+        // left
+        if (col != 0) {
+            int leftIndex = blankTitleIndex - 1;
+            char[] leftSingleDimensionTiles = singleDimensionTiles.clone();
+            // todo hamming + manhattan
+            boolean decrease = leftSingleDimensionTiles[leftIndex] == blankTitleIndex;
+            int hamming = this.hamming + (decrease ? -1 : 1);
+            int manhattan = this.manhattan + (decrease ? -1 : 1);
+            swap(leftSingleDimensionTiles, blankTitleIndex - 1, blankTitleIndex);
+            neighbors.add(new Board(leftSingleDimensionTiles, hamming, manhattan));
+        }
+
+        return neighbors;
     }
 
     // a board that is obtained by exchanging any pair of tiles
@@ -135,25 +188,26 @@ public class Board {
             rightIndex = leftIndex == n2 - 1 ? 0 : leftIndex + 1;
         } while (!isBlankTileIndex(leftIndex) && !isBlankTileIndex(rightIndex));
 
-        Board twin = new Board(this);
-        swap(twin.singleDimensionTiles, leftIndex, rightIndex);
+        char[] twinSingleDimensionTiles = singleDimensionTiles.clone();
+        swap(twinSingleDimensionTiles, leftIndex, rightIndex);
 
-        return twin;
+        // todo hamming + manhattan
+        return new Board(twinSingleDimensionTiles, -1, -1);
     }
 
     private boolean isBlankTileIndex(int index) {
         return isBlankTile(singleDimensionTiles[index]);
     }
 
-    private int toSingleDimension(int row, int col) {
+    private static int toSingleDimension(int row, int col, int n) {
         return row * n + col;
     }
 
-    private int toRow(int k) {
+    private static int toRow(int k, int n) {
         return Math.floorDiv(k, n);
     }
 
-    private int toCol(int k) {
+    private static int toCol(int k, int n) {
         return k % n;
     }
 
@@ -163,6 +217,10 @@ public class Board {
         array[j] = temp;
     }
 
+    private static boolean isBlankTile(char value) {
+        return value == 0;
+    }
+
     // unit testing (not graded)
     public static void main(String[] args) {
         Board b = new Board(new int[][] {
@@ -170,11 +228,13 @@ public class Board {
                 { 4, 5, 6 },
                 { 7, 8, 0 },
                 });
+        // neighbors left (h:1, m:1) top (h:1, m:1)
 
         Board b2 = new Board(new int[][] {
                 { 0, 2 },
                 { 1, 3 },
                 });
+        // neighbors right (h:3, m:3) bottom (h:1, m:1)
 
         // same as b
         Board b3 = new Board(new int[][] {
@@ -188,24 +248,40 @@ public class Board {
                 { 4, 3, 6 },
                 { 8, 7, 5 },
                 });
+        // neighbors right (h:5, m:8) bottom (h:5, m:8)
 
         Board b5 = new Board(new int[][] {
                 { 8, 1, 3 },
                 { 4, 0, 2 },
                 { 7, 6, 5 },
                 });
+        // neighbors left (h:6, m:11) top (h:5, m:11) right (h:5, m:9) bottom (h:5, m:9)
+
+        Board b6 = new Board(new int[][] {
+                { 2, 0, 1 },
+                { 4, 3, 6 },
+                { 8, 7, 5 },
+                });
+        // neighbors left (h:6, m:9) right (h:6, m:8) bottom (h:6, m:8)
+
+        Board b7 = new Board(new int[][] {
+                { 2, 3, 1 },
+                { 4, 7, 6 },
+                { 8, 0, 5 },
+                });
+        // neighbors left top right
 
         // private
         // clone
-        Board bClone = new Board(b);
+        Board bClone = new Board(b.singleDimensionTiles, -1, -1);
         assert bClone != b && bClone.equals(b);
         assert bClone.n == b.n;
         assert bClone.n2 == b.n2;
         assert Arrays.equals(bClone.singleDimensionTiles, b.singleDimensionTiles);
         assert bClone.isGoal == b.isGoal;
-        assert bClone.hamming == b.hamming;
-        assert bClone.manhattan == b.manhattan;
-        assert bClone.boardString == b.boardString;
+        assert bClone.hamming == -1;
+        assert bClone.manhattan == -1;
+        assert bClone.boardString.equals(b.boardString);
 
         // dimension
         assert b.dimension() == 3;
@@ -254,7 +330,11 @@ public class Board {
         assert b4.manhattan() == 8;
         assert b5.manhattan() == 10;
 
+        // neighbors
+        // left
+        assert !b2.neighbors().iterator().hasNext();
+
         // twins
-        // TODO
+        // todo
     }
 }
--- 8puzzle/Solver.java
+++ 8puzzle/Solver.java
@@ -4,16 +4,59 @@
  *  Description:
  **************************************************************************** */
 
-public class Solver {
-    public Solver(Board initial) {
+import edu.princeton.cs.algs4.MinPQ;
 
+import java.util.Comparator;
+
+public class Solver {
+    int moves = 0;
+    Board previous = null;
+    MinPQ<Board> priorityQueue = null;
+    Comparator<Board> HammingComparator = new Comparator<Board>() {
+        public int compare(Board a, Board b) {
+            return a.hamming() - b.hamming();
+        }
+    };
+
+    public Solver(Board initial) {
+        priorityQueue = new MinPQ<>(HammingComparator);
+        priorityQueue.insert(initial);
+        AStrar();
     }
 
+    private boolean AStrar() {
+        while (!priorityQueue.isEmpty()) {
+            Board current = priorityQueue.delMin();
+
+            if (current.isGoal()) return true;
+
+            for (Board neighbor : current.neighbors()) {
+                if (neighbor.equals(previous)) continue;
+
+                priorityQueue.insert(neighbor);
+            }
+        }
+
+        return false;
+    }
+
+    // is the initial board solvable? (see below)
+    public boolean isSolvable() {
+        return true;
+    }
+
+    // min number of moves to solve initial board
+    public int moves() {
+        return moves;
+    }
+
+    // sequence of boards in a shortest solution
+    public Iterable<Board> solution() {
+        return null;
+    }
+
+    // test client (see below)
     public static void main(String[] args) {
 
-    }
-
-    public String moves() {
-        return null;
     }
 }
