e700847e3680bd36e1490f7fb598a201a390186b
Eugene Borys <bogekt@gmail.com>
2020-02-25 01:40:54 +0200

    fix nearest

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -8,10 +8,13 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 import edu.princeton.cs.algs4.SET;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedList;
 
 public class PointSET {
-    private SET<Point2D> points;
+    private final SET<Point2D> points;
 
     // construct an empty set of points
     public PointSET() {
@@ -58,11 +61,39 @@ public class PointSET {
     public Point2D nearest(Point2D p) {
         if (points.isEmpty()) return null;
 
-        Point2D floor = points.floor(p);
-        Point2D celling = points.ceiling(p);
+        ArrayList<Point2D> nearestPoints = new ArrayList<>(points.size());
+        for (Point2D x : points)
+            nearestPoints.add(p);
+
+        Comparator<Point2D> c = new DistanceToOrder(p);
+        nearestPoints.sort(c);
+        int i = Collections.binarySearch(nearestPoints, p, c);
+
+        Point2D floor = i - 1 >= 0 ? nearestPoints.get(i - 1) : null;
+        Point2D celling = i + 1 < nearestPoints.size() ? nearestPoints.get(i + 1) : null;
+        if (floor == null) return celling;
+        else if (celling == null) return floor;
+
         return floor.distanceSquaredTo(p) < celling.distanceSquaredTo(p)
                ? floor
                : celling;
+    }
+
+    // compare points according to their distance to this point
+    private class DistanceToOrder implements Comparator<Point2D> {
+        private final Point2D point;
+
+        public DistanceToOrder(Point2D point) {
+            this.point = point;
+        }
+
+        public int compare(Point2D p, Point2D q) {
+            double dist1 = point.distanceSquaredTo(p);
+            double dist2 = point.distanceSquaredTo(q);
+            if (dist1 < dist2) return -1;
+            else if (dist1 > dist2) return +1;
+            else return 0;
+        }
     }
 
     // unit testing of the methods (optional)
