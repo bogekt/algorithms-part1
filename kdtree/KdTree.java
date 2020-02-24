119198fcbd1800a7cafc05721ad6d3f5dd244fe2
Eugene Borys <bogekt@gmail.com>
2020-02-25 00:39:04 +0200

    paste required api

--- kdtree/KdTree.java
+++ kdtree/KdTree.java
@@ -8,23 +8,44 @@ import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
 
 public class KdTree {
-    public static void main(String[] args) {
-
+    // construct an empty set of points
+    public KdTree() {
     }
 
+    // is the set empty?
+    public boolean isEmpty() {
+        return false;
+    }
+
+    // number of points in the set
+    public int size() {
+        return 0;
+    }
+
+    // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
-
     }
 
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    // draw all points to standard draw
     public void draw() {
-
     }
 
-    public KdTree nearest(Point2D p) {
+    // all points that are inside the rectangle (or on the boundary)
+    public Iterable<Point2D> range(RectHV rect) {
         return null;
     }
 
-    public Iterable<? extends Point2D> range(RectHV rect) {
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
         return null;
+    }
+
+    // unit testing of the methods (optional)
+    public static void main(String[] args) {
     }
 }
