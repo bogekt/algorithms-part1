524aaaa3bef173def1ceecda4aed49ec487de0fb
Eugene Borys <bogekt@gmail.com>
2020-02-25 01:06:22 +0200

    naive implementation of PointSET

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -6,43 +6,63 @@
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.SET;
+
+import java.util.LinkedList;
 
 public class PointSET {
+    private SET<Point2D> points;
+
     // construct an empty set of points
     public PointSET() {
+        points = new SET<Point2D>();
     }
 
     // is the set empty?
     public boolean isEmpty() {
-        return false;
+        return points.isEmpty();
     }
 
     // number of points in the set
     public int size() {
-        return 0;
+        return points.size();
     }
 
     // add the point to the set (if it is not already in the set)
     public void insert(Point2D p) {
+        points.add(p);
     }
 
     // does the set contain point p?
     public boolean contains(Point2D p) {
-        return false;
+        return points.contains(p);
     }
 
     // draw all points to standard draw
     public void draw() {
+        for (Point2D p : points)
+            p.draw();
     }
 
     // all points that are inside the rectangle (or on the boundary)
     public Iterable<Point2D> range(RectHV rect) {
-        return null;
+        LinkedList<Point2D> inside = new LinkedList<>();
+
+        for (Point2D p : points)
+            if (rect.contains(p)) inside.add(p);
+
+        return inside;
     }
 
     // a nearest neighbor in the set to point p; null if the set is empty
     public Point2D nearest(Point2D p) {
-        return null;
+        if (points.isEmpty()) return null;
+
+        Point2D floor = points.floor(p);
+        Point2D celling = points.ceiling(p);
+        return floor.distanceSquaredTo(p) < celling.distanceSquaredTo(p)
+               ? floor
+               : celling;
     }
 
     // unit testing of the methods (optional)
