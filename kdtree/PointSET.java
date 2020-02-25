ef191e9c23e8544a4a203f0cf7ad56fa09fcaeec
Eugene Borys <bogekt@gmail.com>
2020-02-26 00:22:23 +0200

    fix nearest equals

--- kdtree/PointSET.java
+++ kdtree/PointSET.java
@@ -62,11 +62,11 @@ public class PointSET {
     public Point2D nearest(Point2D p) {
         if (p == null) throw new IllegalArgumentException();
         if (points.isEmpty()) return null;
+        if (points.contains(p)) return p;
 
         Point2D min = null;
 
         for (Point2D q : points) {
-            if (q.equals(p)) continue;
             if (min != null && p.distanceSquaredTo(min) <= p.distanceSquaredTo(q)) continue;
             min = q;
         }
@@ -76,27 +76,85 @@ public class PointSET {
 
     // unit testing of the methods (optional)
     public static void main(String[] args) {
+        testNearest(
+                new Point2D[] {
+                        /* A */ new Point2D(0.5, 0.0),
+                        /* B */ new Point2D(1.0, 0.5),
+                        /* C */ new Point2D(0.5, 0.0),
+                        /* D */ new Point2D(0.25, 0.75),
+                        /* E */ new Point2D(0.75, 0.0),
+                        /* F */ new Point2D(0.25, 1.0),
+                        /* G */ new Point2D(1.0, 0.0),
+                        /* H */ new Point2D(0.5, 0.25),
+                        /* I */ new Point2D(0.5, 0.5),
+                        /* J */ new Point2D(0.0, 0.5),
+                        },
+                new Point2D(0.5, 0.75),
+                new Point2D(0.5, 0.5),
+                0.0625
+        );
+
+        testNearest(
+                new Point2D[] {
+                        /* A */  new Point2D(1.0, 1.0),
+                        /* B */  new Point2D(0.75, 0.75),
+                        /* C */  new Point2D(1.0, 1.0),
+                        /* D */  new Point2D(0.5, 0.25),
+                        /* E */  new Point2D(1.0, 0.25),
+                        /* F */  new Point2D(0.75, 1.0),
+                        /* G */  new Point2D(0.0, 1.0),
+                        /* H */  new Point2D(0.0, 0.5),
+                        /* I */  new Point2D(0.0, 0.0),
+                        /* J */  new Point2D(0.0, 0.25),
+                        },
+                new Point2D(0.75, 1.0),
+                new Point2D(0.75, 1.0),
+                0
+        );
+
+        testNearest(
+                new Point2D[] {
+                        /* A */  new Point2D(1.0, 0.9375),
+                        /* B */  new Point2D(0.1875, 0.1875),
+                        /* C */  new Point2D(0.6875, 0.6875),
+                        /* D */  new Point2D(0.0625, 0.0625),
+                        /* E */  new Point2D(0.0625, 0.9375),
+                        /* F */  new Point2D(0.0625, 0.875),
+                        /* G */  new Point2D(0.125, 0.8125),
+                        /* H */  new Point2D(0.1875, 0.0),
+                        /* I */  new Point2D(0.3125, 0.1875),
+                        /* J */  new Point2D(0.1875, 0.4375),
+                        /* K */  new Point2D(0.9375, 0.8125),
+                        /* L */  new Point2D(0.9375, 0.875),
+                        /* M */  new Point2D(0.3125, 0.0625),
+                        /* N */  new Point2D(1.0, 0.9375),
+                        /* O */  new Point2D(0.5625, 0.6875),
+                        /* P */  new Point2D(0.4375, 0.9375),
+                        /* Q */  new Point2D(0.125, 0.625),
+                        /* R */  new Point2D(0.9375, 0.9375),
+                        /* S */  new Point2D(0.125, 0.1875),
+                        /* T */  new Point2D(0.0, 0.5625),
+                        },
+                new Point2D(0.3125, 0.1875),
+                new Point2D(0.3125, 0.1875),
+                0
+        );
+    }
+
+    private static void testNearest(
+            Point2D[] insert,
+            Point2D query,
+            Point2D expected,
+            double expectedDistance
+    ) {
         PointSET pointSET = new PointSET();
 
-        for (Point2D p : new Point2D[] {
-                /*A*/ new Point2D(0.5, 0.0),
-                /*B*/ new Point2D(1.0, 0.5),
-                /*C*/ new Point2D(0.5, 0.0),
-                /*D*/ new Point2D(0.25, 0.75),
-                /*E*/ new Point2D(0.75, 0.0),
-                /*F*/ new Point2D(0.25, 1.0),
-                /*G*/ new Point2D(1.0, 0.0),
-                /*H*/ new Point2D(0.5, 0.25),
-                /*I*/ new Point2D(0.5, 0.5),
-                /*J*/ new Point2D(0.0, 0.5),
-                })
+        for (Point2D p : insert)
             pointSET.insert(p);
 
-        Point2D expected = new Point2D(0.5, 0.5);
-        Point2D query = new Point2D(0.5, 0.75);
         Point2D nearest = pointSET.nearest(query);
 
         assert nearest.equals(expected);
-        assert nearest.distanceSquaredTo(query) == 0.0625;
+        assert nearest.distanceSquaredTo(query) == expectedDistance;
     }
 }
